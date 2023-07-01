54f4c55dbb8f1dae8ed60a1879f6b9b12d418499
Areebur Rahman <102393675+AreeburR@users.noreply.github.com>
2023-07-01 16:34:30 -0400

    new items are able to be created and removed

--- index.js
+++ index.js
@@ -28,3 +28,69 @@ for (let button of removeButtons) {
     });
 }
 
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let name = e.target.name.value;
+    let image = e.target.image.value;
+    let quantity = e.target.quantity.value;
+    let price = e.target.price.value;
+    let stock = e.target.stock.value;
+
+    createSnack(name, image, quantity, price, stock);
+    form.reset();
+});
+
+function snackTemplate(name, image, quantity, price, stock) {
+    let li = document.createElement("li");
+    let h2 = document.createElement("h2");
+    h2.textContent = name;
+    li.append(h2, document.createElement("hr"));
+    let img = document.createElement("img");
+    img.src = image;
+    let quantityP = document.createElement("p");
+    quantityP.textContent = `Minimum Quantity: ${quantity}`;
+    let priceP = document.createElement("p");
+    priceP.textContent = `Price: $${price}`;
+    li.append(img, quantityP, priceP);
+
+
+    let inStockButton = document.createElement("button");
+    inStockButton.setAttribute("id", "inStock");
+    inStockButton.textContent = "In Stock";
+
+    let removeButton = document.createElement("button");
+    removeButton.setAttribute("id", "remove");
+    removeButton.textContent = "Remove";
+    li.append(inStockButton, document.createElement("br"), removeButton);
+
+    let inStock = true;
+    inStockButton.addEventListener("click", (e) => {
+        e.preventDefault();
+        inStock = !inStock;
+        if (inStock === true) {
+            inStockButton.textContent = "In Stock"
+            inStockButton.style.background = "green"
+            inStockButton.style.textcolor = "black"
+        } else {
+            inStockButton.textContent = "Out of Stock"
+            inStockButton.style.background = "red"
+            inStockButton.style.textcolor = "white"
+        }
+    });
+
+    removeButton.addEventListener("click", (e) => {
+        li.remove();
+    });
+
+    return li;
+}
+
+function createSnack(name, image, quantity, price, stock){
+    const ul = document.querySelector("ul");
+    const li = snackTemplate(name, image, quantity, price, stock);
+
+    ul.prepend(li);
+}
