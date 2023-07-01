b45deafa45b4bf8089a23d8c8bed0666a679d7e3
Areebur Rahman <102393675+AreeburR@users.noreply.github.com>
2023-07-01 16:53:33 -0400

    readme completed

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# inventory-web-application-project-areebur
+# Inventory-web-application-project-areebur
+
+## About
+---
+This Pursuit project is about creating an inventory application that handle the creation and deletion of products, in this case snacks.
+
+## How To Use
+---
+
+Here is the example product criteria used in testing.
+
+name: Sun Chips
+
+minimum quantity: 5
+
+image url: https://www.freshdirect.com/media/images/product/grocery_55/gro_pid_4018604_j.jpg?lastModify=2022-08-09&publishId=3220
+
+price: 4
+
+Upon filling out the form and hitting submit a new item is prepended to the current existing list.
+
+
+## Stretch Goal
+---
+I deployed the web page on Github
--- index.js
+++ index.js
@@ -3,94 +3,93 @@ const stockButtons = document.querySelectorAll("#inStock");
 let inStock = true;
 
 for (let button of stockButtons) {
-    button.addEventListener("click", (e) => {
-        e.preventDefault();
-        inStock = !inStock;
-        if (inStock === true) {
-            button.textContent = "In Stock"
-            button.style.background = "green"
-            button.style.textcolor = "black"
-        } else {
-            button.textContent = "Out of Stock"
-            button.style.background = "red"
-            button.style.textcolor = "white"
-        }
-    });
+  button.addEventListener("click", (e) => {
+    e.preventDefault();
+    inStock = !inStock;
+    if (inStock === true) {
+      button.textContent = "In Stock";
+      button.style.background = "green";
+      button.style.textcolor = "black";
+    } else {
+      button.textContent = "Out of Stock";
+      button.style.background = "red";
+      button.style.textcolor = "white";
+    }
+  });
 }
 
 const removeButtons = document.querySelectorAll("#remove");
 
 for (let button of removeButtons) {
-    button.addEventListener("click", (e) => {
-        e.preventDefault();
-        e.target.parentElement.remove();
-        e.reset();
-    });
+  button.addEventListener("click", (e) => {
+    e.preventDefault();
+    e.target.parentElement.remove();
+    e.reset();
+  });
 }
 
 const form = document.querySelector("form");
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    let name = e.target.name.value;
-    let image = e.target.image.value;
-    let quantity = e.target.quantity.value;
-    let price = e.target.price.value;
-    let stock = e.target.stock.value;
+  let name = e.target.name.value;
+  let image = e.target.image.value;
+  let quantity = e.target.quantity.value;
+  let price = e.target.price.value;
+  let stock = e.target.stock.value;
 
-    createSnack(name, image, quantity, price, stock);
-    form.reset();
+  createSnack(name, image, quantity, price, stock);
+  form.reset();
 });
 
 function snackTemplate(name, image, quantity, price, stock) {
-    let li = document.createElement("li");
-    let h2 = document.createElement("h2");
-    h2.textContent = name;
-    li.append(h2, document.createElement("hr"));
-    let img = document.createElement("img");
-    img.src = image;
-    let quantityP = document.createElement("p");
-    quantityP.textContent = `Minimum Quantity: ${quantity}`;
-    let priceP = document.createElement("p");
-    priceP.textContent = `Price: $${price}`;
-    li.append(img, quantityP, priceP);
+  let li = document.createElement("li");
+  let h2 = document.createElement("h2");
+  h2.textContent = name;
+  li.append(h2, document.createElement("hr"));
+  let img = document.createElement("img");
+  img.src = image;
+  let quantityP = document.createElement("p");
+  quantityP.textContent = `Minimum Quantity: ${quantity}`;
+  let priceP = document.createElement("p");
+  priceP.textContent = `Price: $${price}`;
+  li.append(img, quantityP, priceP);
 
+  let inStockButton = document.createElement("button");
+  inStockButton.setAttribute("id", "inStock");
+  inStockButton.textContent = "In Stock";
 
-    let inStockButton = document.createElement("button");
-    inStockButton.setAttribute("id", "inStock");
-    inStockButton.textContent = "In Stock";
+  let removeButton = document.createElement("button");
+  removeButton.setAttribute("id", "remove");
+  removeButton.textContent = "Remove";
+  li.append(inStockButton, document.createElement("br"), removeButton);
 
-    let removeButton = document.createElement("button");
-    removeButton.setAttribute("id", "remove");
-    removeButton.textContent = "Remove";
-    li.append(inStockButton, document.createElement("br"), removeButton);
+  let inStock = true;
+  inStockButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    inStock = !inStock;
+    if (inStock === true) {
+      inStockButton.textContent = "In Stock";
+      inStockButton.style.background = "green";
+      inStockButton.style.textcolor = "black";
+    } else {
+      inStockButton.textContent = "Out of Stock";
+      inStockButton.style.background = "red";
+      inStockButton.style.textcolor = "white";
+    }
+  });
 
-    let inStock = true;
-    inStockButton.addEventListener("click", (e) => {
-        e.preventDefault();
-        inStock = !inStock;
-        if (inStock === true) {
-            inStockButton.textContent = "In Stock"
-            inStockButton.style.background = "green"
-            inStockButton.style.textcolor = "black"
-        } else {
-            inStockButton.textContent = "Out of Stock"
-            inStockButton.style.background = "red"
-            inStockButton.style.textcolor = "white"
-        }
-    });
+  removeButton.addEventListener("click", (e) => {
+    li.remove();
+  });
 
-    removeButton.addEventListener("click", (e) => {
-        li.remove();
-    });
-
-    return li;
+  return li;
 }
 
-function createSnack(name, image, quantity, price, stock){
-    const ul = document.querySelector("ul");
-    const li = snackTemplate(name, image, quantity, price, stock);
+function createSnack(name, image, quantity, price, stock) {
+  const ul = document.querySelector("ul");
+  const li = snackTemplate(name, image, quantity, price, stock);
 
-    ul.prepend(li);
+  ul.prepend(li);
 }
